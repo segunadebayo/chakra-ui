65534faf25c888efa2c8e886b85d639ac08dbe65
Tioluwani Kolawole <kolawole8tiolu@gmail.com>
2019-08-03 09:11:07 +0100

    complete type definitions for components

--- src/Center/index.d.ts
+++ src/Center/index.d.ts
@@ -1,7 +1,7 @@
 import { ForwardRefExoticComponent, RefAttributes } from "react";
 import { BoxProps } from "../Box";
 
-type CenterProps = BoxProps & RefAttributes<HTMLDivElement>;
+export type CenterProps = BoxProps & RefAttributes<HTMLDivElement>;
 
 declare const Center: ForwardRefExoticComponent<CenterProps>;
 
